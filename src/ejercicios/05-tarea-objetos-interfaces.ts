e970ed0987336bdad90b01a69a253b0e1070e04c
alainhortiz <alainhortiz@gmail.com>
2023-11-24 17:27:25 -0500

    Tarea sobre objetos e interfases

---
+++ src/ejercicios/05-tarea-objetos-interfaces.ts
@@ -0,0 +1,48 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface SuperHeroe {
+    nombre: string;
+    edad: number;
+    direccion: Direccion,
+    mostrarDireccion: () => string;
+}
+
+interface Direccion {
+    calle: string;
+    pais: string;
+    ciudad: string;
+    alcalde: SuperHeroe
+}
+
+const superHeroe: SuperHeroe = {
+    nombre: 'Superman',
+    edad: 30,
+    direccion: {
+        calle: 'Calle 123',
+        pais: 'México',
+        ciudad: 'Ciudad de México'
+        alcalde: {
+            nombre: 'Gonzalo',
+            edad: 20,
+            direccion: {
+                calle: 'Calle 123',
+                pais: 'México',
+                ciudad: 'Ciudad de México'
+            }
+        }
+    },
+    mostrarDireccion(): string {
+        return this.nombre + ', de ' + this.edad + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais + ' y el alcalde es ' + this.direccion.alcalde.nombre;
+    }
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
+
+
+
+
+
