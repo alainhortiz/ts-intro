1c26f6fbcb1ec46cdfbe2cb2ddb877f6a9644bea
alainhortiz <alainhortiz@gmail.com>
2023-11-24 13:48:35 -0500

    Tipos Básicos

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "Juan";
+let hp: string|number = 95;
+let estaVivo: boolean = true;
+
+hp = 'Juan';
+
+console.log(typeof nombre, typeof hp, typeof estaVivo);
