6cabcb492665b8b8c498eef5f4dc8660d23b828b
alainhortiz <alainhortiz@gmail.com>
2023-11-24 20:48:01 -0500

    Importaciones y exportaciones

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,4 @@
 /*
     ===== Código de TypeScript =====
 */
-///Importacion a traves de la desestructuración de argumentos de la interface Producto
-import { Producto, calculaImpuestoSobreVenta } from "./ejercicios/08-desestructuracion-argumentos";
-
-
-const carritoCompras: Producto[] = [
-    {
-        desc: 'Telefono 1',
-        precio: 150
-    },
-    {
-        desc: 'Telefono 2',
-        precio: 280
-    },
-];
-
-const [total, isv] = calculaImpuestoSobreVenta( carritoCompras );
-
-console.log('Total:',total);
-console.log('ISV:',isv);
-
 
